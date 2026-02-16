38eaf44fbb6ba601a66c4059b72d721a5aae182a
Mario <mariojrsaraosos@gmail.com>
2026-02-16 10:23:34 +0800

    Add files via upload

---
+++ maria/sar/mar/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MarConfig(AppConfig):
+    name = 'mar'
---
+++ maria/sar/mar/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from requests import request
+def home(request):
+    return render(request, 'home.html')
+def contact(request):
+    return render(request, 'contact.html')
+def about(request):
+    return render(request, 'about.html')
+ 
